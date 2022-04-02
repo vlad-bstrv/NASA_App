edd2ddf42168301093f276ed92c6c520877e180c
vlad-bstrv <keocera@icloud.com>
2022-04-02 16:26:14 +0300

    ДЗ 7. Добавил шрифт RubikGlitch-Regular  в папку asset и через среду разработки montserrat_bold. Оба применил к bottomSheetDescriptionHeader

--- app/src/main/java/com/vladbstrv/nasaapp/view/picture_of_the_day/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/vladbstrv/nasaapp/view/picture_of_the_day/PictureOfTheDayFragment.kt
@@ -1,10 +1,13 @@
 package com.vladbstrv.nasaapp.view.picture_of_the_day
 
 import android.content.Intent
+import android.graphics.Typeface
 import android.net.Uri
 import android.os.Build
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.*
 import android.view.animation.AnticipateOvershootInterpolator
 import android.widget.Toast
@@ -13,6 +16,8 @@ import androidx.fragment.app.Fragment
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.ContextCompat
+import androidx.core.provider.FontRequest
+import androidx.core.provider.FontsContractCompat
 import androidx.lifecycle.Observer
 import androidx.transition.ChangeBounds
 import androidx.transition.TransitionManager
@@ -158,6 +163,7 @@ class PictureOfTheDayFragment : Fragment() {
                 binding.imageView.load(pictureOfTheDayState.serverResponseData.url)
                 binding.included.bottomSheetDescriptionHeader.text =
                     pictureOfTheDayState.serverResponseData.title
+                setFontAndText()
                 binding.included.bottomSheetDescription.text =
                     pictureOfTheDayState.serverResponseData.explanation
 
@@ -165,6 +171,30 @@ class PictureOfTheDayFragment : Fragment() {
                 binding.tvDescription.text = pictureOfTheDayState.serverResponseData.explanation
             }
         }
+    }
+
+    private fun setFontAndText() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            binding.included.bottomSheetDescriptionHeader.typeface = Typeface.createFromAsset(requireActivity().assets,
+            "RubikGlitch-Regular.ttf")
+        }
+
+        val request = FontRequest(
+            "com.google.android.gms.fonts",
+            "com.google.android.gms",
+            "name=Montserrat&amp;weight=700",
+            R.array.com_google_android_gms_fonts_certs
+        )
+
+        val callback = object : FontsContractCompat.FontRequestCallback() {
+            override fun onTypefaceRetrieved(typeface: Typeface?) {
+                binding.included.bottomSheetDescriptionHeader.typeface = typeface
+                super.onTypefaceRetrieved(typeface)
+            }
+        }
+
+        FontsContractCompat.requestFont(requireContext(), request, callback, Handler(Looper.myLooper()!!))
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
